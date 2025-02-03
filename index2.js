827501c7c270e3679742bd3f42756aa8b9b7cde7
calayo3 <alayodelatorrecesar@fpllefia.com>
2025-02-03 16:32:18 +0000

    tuto2

---
+++ index2.js
@@ -0,0 +1,18 @@
+console.log("Bienvenido index2.js")
+let arrayFrutas = ["naranja", "pera", "Fresa"]
+console.log("Mi fruta favorita es: ", arrayFrutas[2])
+
+for(let i=0; i < 3; i++){
+    console.log(arrayFrutas[i])
+    i++
+}
+
+let botton =document.querySelector(contenido3>button)
+console.log(botton.innerHTML)
+
+botton.addEventListener("click", cambiacolor)
+
+function cambiacolor(){
+    botton.classList.add("colorgreen")
+    contenido1
+}
